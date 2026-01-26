7e5b2143252ff2be9ce0dc8ebe7b9c12f0d1a811
Andrea Borbon <adp.borbon@gmail.com>
2026-01-26 17:50:49 +0100

    Updated input parameters

--- scripts/bash/run_nanoplot.sh
+++ scripts/bash/run_nanoplot.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 
-#Usage ./run_nanoplot.sh <INPUT_FOLDER>
-#Example: ./run_nanoplot.sh 251117_fastq_supAcc
-#
+#Usage ./run_nanoplot.sh <INPUT_FOLDER> <OUTPUT_FOLDER> <LOG_FILE>
+#Example: ./run_nanoplot.sh 251117_fastq_supAcc 251117_NanoPlot_fastq_supAcc_batch6
 
+#Input arguments
 INPUT_FOLDER="$1"
+OUTPUT_FOLDER="$2"
+LOG_FILE="$3"
 
-echo -e "sample\telapsed_seconds" > timelog.tsv
+#Create output folder if it does not exist
+mkdir -p $OUTPUT_FOLDER
 
+#Initialize log file
+echo -e "sample\telapsed_seconds" > $LOG_FILE
+
+#Run NanoPlot in parallel for all fastq.gz files in the input folder
 ls $INPUT_FOLDER/*.fastq.gz | parallel '
     sample={/.}
     start=$(date +%s)
 
     NanoPlot --fastq {} \
-             --outdir 251117_NanoPlot_fastq_supAcc_batch6/$sample \
+             --outdir $OUTPUT_FOLDER/$sample \
              --title $sample \
              -t 10 \
              --prefix ${sample}_
@@ -21,5 +28,7 @@ ls $INPUT_FOLDER/*.fastq.gz | parallel '
     end=$(date +%s)
     elapsed=$((end - start))
 
-    echo -e "$sample\t$elapsed" >> timelog.tsv
+    echo -e "$sample\t$elapsed" >> $LOG_FILE
+    mv $LOG_FILE $OUTPUT_FOLDER/$LOG_FILE
+
 '
