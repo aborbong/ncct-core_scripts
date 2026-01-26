5c52b0853c227ba4f8e9936fa70045442323766a
Andrea Borbon <adp.borbon@gmail.com>
2026-01-26 22:02:44 +0100

    reverted to previous working version

--- scripts/bash/run_nanoplot.sh
+++ scripts/bash/run_nanoplot.sh
@@ -9,27 +9,22 @@ INPUT_FOLDER="$1"
 OUTPUT_FOLDER="$2"
 LOG_FILE="$3"
 
-#Initialize log file
-echo -e "sample\telapsed_seconds" > "$LOG_FILE"
+INPUT_FOLDER="$1"
 
-#Create output folder if it does not exist
-mkdir -p "$OUTPUT_FOLDER"
+echo -e "sample\telapsed_seconds" > timelog.tsv
 
-export OUTPUT_FOLDER LOG_FILE
-
-#Run NanoPlot in parallel for all fastq.gz files in the input folder
-parallel --env OUTPUT_FOLDER, LOG_FILE '
-    sample=$(basename {} .fastq.gz)
-    start=$(date +%s);
+ls $INPUT_FOLDER/*.fastq.gz | parallel '
+    sample={/.}
+    start=$(date +%s)
 
     NanoPlot --fastq {} \
-             --outdir "$OUTPUT_FOLDER/${sample}" \
-             --title "${sample}" \
+             --outdir 251117_NanoPlot_fastq_supAcc_batch6/$sample \
+             --title $sample \
              -t 10 \
-             --prefix "${sample}_"
+             --prefix ${sample}_
 
     end=$(date +%s)
     elapsed=$((end - start))
 
-    echo -e "$sample\t$elapsed" >> "$LOG_FILE"
-' ::: "$INPUT_FOLDER"/*.fastq.gz
+    echo -e "$sample\t$elapsed" >> timelog.tsv
+'
