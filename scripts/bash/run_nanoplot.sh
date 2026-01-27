786be8f89eb601c3d43394869fd5c250349ca0b1
Andrea Borbon <adp.borbon@gmail.com>
2026-01-27 19:04:09 +0100

    fix GNU parallel variable and logging

--- scripts/bash/run_nanoplot.sh
+++ scripts/bash/run_nanoplot.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
-#Usage ./run_nanoplot.sh <INPUT_FOLDER>
-#Example: ./run_nanoplot.sh 251117_fastq_supAcc
-#
+#Usage ./run_nanoplot.sh <INPUT_FOLDER> <OUTPUT_FOLDER>
+#Example: ./run_nanoplot.sh 251117_fastq_supAcc 251117_NanoPlot_fastq_supAcc
 
 INPUT_FOLDER="$1"
+OUTPUT_FOLDER="$2"
 
-echo -e "sample\telapsed_seconds" > timelog.tsv
+mkdir -p "$OUTPUT_FOLDER"
 
-ls $INPUT_FOLDER/*.fastq.gz | parallel '
+echo -e "sample\telapsed_seconds" > timelog.tsv | tee -a timelog.tsv >/dev/null
+
+export OUTPUT_FOLDER
+
+shopt -s nullglob
+files=("$INPUT_FOLDER"/*.fastq.gz)
+
+
+printf '%s\n' "${files[@]}" | parallel --env OUTPUT_FOLDER '
     sample={/.}
     start=$(date +%s)
 
     NanoPlot --fastq {} \
-             --outdir 251117_NanoPlot_fastq_supAcc_batch6/$sample \
+             --outdir "$OUTPUT_FOLDER"/$sample \
              --title $sample \
              -t 10 \
              --prefix ${sample}_
@@ -21,5 +29,5 @@ ls $INPUT_FOLDER/*.fastq.gz | parallel '
     end=$(date +%s)
     elapsed=$((end - start))
 
-    echo -e "$sample\t$elapsed" >> timelog.tsv
-'
+    echo -e "$sample\t$elapsed" 
+' >> timelog.tsv
